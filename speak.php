dbf1ab9420f2cbf5b3a41d234e5c5c5aa35e328f
imageny <62641690+imageny@users.noreply.github.com>
2020-05-01 02:54:54 -0500

    Update speak.php

--- speak.php
+++ speak.php
@@ -2,19 +2,19 @@
 date_default_timezone_set('America/Chicago');
    $tts = 'Command error in q parameter.';
    
-   $truncate = substr($tts, 0, 15);
-   
-   if ($_GET['q']) {
+    if ($_GET['q']) {
 	 $tts = $_GET['q'];
     }
+   
+   $truncate = substr($tts, 0, 15);
+   
+   $file = "dectalk/speech." . md5($truncate) . "." . date("mdyhisA") . ".wav";
 
-$file = "dectalk/speech." . md5($truncate) . "." . date("mdyhisA") . ".wav";
-
-$filename = "speech." . md5($truncate) . "." . date("mdyhisA") . ".wav ";
+   $filename = "speech." . md5($truncate) . "." . date("mdyhisA") . ".wav ";
 
 	if (!file_exists($file)) {
-     $command = "say.exe -w " . $filename . "[:phoneme on]" . $tts;
-     shell_exec("cd dectalk && ".$command);
+     $command = "cd dectalk && say.exe -w " . $filename . "[:phoneme on]" . $tts;
+     shell_exec($command);
 	}
 	
 	if (file_exists($file)) {
@@ -25,3 +25,4 @@ function playFile($file){
 	header('Content-Type: audio/x-wav');
 	readfile($file);
 }
+?>
